f88e5a18ad02fbc19b4c5c7a67220a8ce06a1194
Administrator <576303283@qq.com>
2021-05-05 22:46:35 +0800

    寻找本地缓存包路径

--- models/package/lib/index.js
+++ models/package/lib/index.js
@@ -5,7 +5,7 @@ const formatPath = require('@paxiong-cli/format-path')
 const path = require('path')
 const npminstall = require('npminstall')
 const getNpmInfo = require('@paxiong-cli/get-npm-info')
-
+const pathExists = require('path-exists').sync
 class Package {
 	constructor(options) {
 		if (!options && Object.prototype.toString.call(options) !== "[object Object]") {
@@ -20,13 +20,34 @@ class Package {
 		 this.packageName = options.packageName
 		 // package的version
 		 this.packageVersion = options.packageVersion
+		 // package的缓存前缀
+		 this.cacheFilePathPrefix = this.packageName.replace('/', '_')
+	}
+
+	async prepare() {
+		if (this.packageVersion === 'latest') {
+			this.packageVersion = await getNpmInfo.getNpmLatestVersion(this.packageName)
+		}
+	}
+
+	// _ccc@0.5.1@ccc
+	get cacheFilePath() {
+		return path.resolve(this.storePath, `_${this.cacheFilePathPrefix}@${this.packageVersion}@${this.packageName}`)
 	}
 
 	// 判断当前Package是否存在
-	exists() {}
+	async exists() {
+		if (this.storePath) {
+			await this.prepare()
+			return pathExists(this.cacheFilePath)
+		} else {
+			return pathExists(this.targetPath)
+		}
+	}
 
 	// 安装Package
 	async install() {
+		await this.prepare()
 		return npminstall({
 			root: this.targetPath,
 			storeDir: this.storePath,
@@ -44,7 +65,6 @@ class Package {
 	getRootFilePath() {
 		// 获取pakage.json所在的目录
 		const dir = pkgDir.sync(this.targetPath)
-
 		if (dir) {
 			// 读取package.json
 			const pkgFile = require(path.resolve(dir, 'package.json'))
