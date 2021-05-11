55445d006a130d396da6f809dd2a68609e628fec
Administrator <576303283@qq.com>
2021-05-11 23:03:15 +0800

    完善command模块

--- commands/init/lib/index.js
+++ commands/init/lib/index.js
@@ -1,9 +1,18 @@
 'use strict';
 
 const Command = require('@paxiong-cli/command')
-
+const log = require('@paxiong-cli/log')
 class InitCommand extends Command {
+  init() {
+    this.projectName = this._argv[0] || ''
+    this.force = !!this._argv[1]
+    log.verbose('projectName', this.projectName)
+    log.verbose('force', this.force)
+  }
 
+  exec() {
+    console.log(55)
+  }
 }
 
 function init(argv) {
